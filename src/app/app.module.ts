4161b7f63d688497acb653acd73bbf8444aa70cd
mviniccius <marcus.viniccius@hotmail.com>
2024-05-14 20:48:56 -0300

    Adicionando imagem, imports de modulos do primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/page/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button'
+import { ToastModule } from 'primeng/toast'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    ReactiveFormsModule,
+    //PrimeNg
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
 
   ],
   providers: [],
