0666429e42e1d92b02378b38f8ad2022284b7832
mviniccius <marcus.viniccius@hotmail.com>
2024-05-16 09:41:05 -0300

    Adicionado funcoes de 'criarconta' e 'fazerlogin' utilizando o FormBuilder

--- src/app/modules/page/home/home.component.ts
+++ src/app/modules/page/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email:['',Validators.required],
+    password:['',Validators.required]
+
+  });
+
+  singupForm = this.formBuilder.group({
+    name:['',Validators.required],
+    email:['',Validators.required],
+    password:['',Validators.required]
+  })
+
+  constructor(private formBuilder: FormBuilder){}
+
+  onSubmitLoginForm():void{
+    console.log("Dados de login" + this.loginForm.value)
+  }
+
+  onSubmitSingupForm():void{
+    console.log("Criação de conta" + this.singupForm.value)
+    this.loginCard = true;
+  }
 }
